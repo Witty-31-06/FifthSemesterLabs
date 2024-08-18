2b1cade2236080412c0265d37d36e02286f0b52d
Witty-31-06 <92244610+Witty-31-06@users.noreply.github.com>
2024-08-18 22:41:54 +0530

    BASIC CODE REFACTORING

--- CN_Lab/Assignment1/server.py
+++ CN_Lab/Assignment1/server.py
@@ -7,10 +7,10 @@ from crc import validate_crc_codeword
 
 # Define the server address and port
 SERVER_ADDRESS = 'localhost'
-SERVER_PORT = 12345
+# SERVER_PORT = 12345
 
-def validate_packet(packet, technique):
-    # Validate codeword based on the specified technique
+def check_packet(packet, technique):
+
     if technique == 'checksum':
         return validate_checksum_codeword(packet)
     elif technique in ['crc-8', 'crc-10', 'crc-16', 'crc-32']:
@@ -18,33 +18,38 @@ def validate_packet(packet, technique):
     else:
         raise ValueError("Invalid technique specified")
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python server.py <technique>")
-        sys.exit(1)
 
-    technique = sys.argv[1].lower()
+if len(sys.argv) != 3:
+    print("Usage: python server.py <port> <technique>")
+    sys.exit(1)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((SERVER_ADDRESS, SERVER_PORT))
-        s.listen()
+technique = sys.argv[2].lower()
+SERVER_PORT = int(sys.argv[1])
+if SERVER_PORT < 1024 or SERVER_PORT > 65535:
+    print("Port number must be between 1024 and 65535")
+    sys.exit(1)
+if technique not in ['checksum', 'crc-8', 'crc-10', 'crc-16', 'crc-32']:
+    print("Invalid technique specified")
+    sys.exit(1)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((SERVER_ADDRESS, SERVER_PORT))
+    s.listen()
 
-        print(f"Server is listening on {SERVER_ADDRESS}:{SERVER_PORT}...")
+    print(f"Server is listening on {SERVER_ADDRESS}:{SERVER_PORT}...")
 
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}")
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
 
-                while True:
-                    data = conn.recv(1024).decode()
-                    if not data:
-                        break
+            while True:
+                data = conn.recv(1024).decode()
+                if not data:
+                    break
 
-                    is_valid = validate_packet(data, technique)
-                    response = '1' if is_valid else '0'
-                    conn.sendall(response.encode())
-                print(f"Connection closed by {addr}")
+                is_valid = check_packet(data, technique)
+                response = 'ACK' if is_valid else 'NAK'
+                conn.sendall(response.encode())
+            print(f"Connection closed by {addr}")
 
-if __name__ == "__main__":
-    main()
